0bea5bda60d70b48581d7dd68221878d10b6980a
Ваше Имя <you@example.com>
2020-10-08 15:49:06 +0300

    Исправление ошибок с извлечением текстом из пдф

--- pdf_parser/slide_splitter.py
+++ pdf_parser/slide_splitter.py
@@ -1,37 +1,20 @@
-import io
+import fitz
+
 import os
 import argparse
-
-from pdfminer.converter import TextConverter
-from pdfminer.pdfinterp import PDFPageInterpreter
-from pdfminer.pdfinterp import PDFResourceManager
-from pdfminer.pdfpage import PDFPage
 
 
 def extract_text_by_page(pdf_path, extract_dir):
     if not os.path.exists(extract_dir):
         os.mkdir(extract_dir)
 
-    with open(pdf_path, 'rb') as fh:
-         page_num = 0
-         for page in PDFPage.get_pages(fh,
-                                  caching=True,
-                                  check_extractable=True):
-            resource_manager = PDFResourceManager()
-            fake_file_handle = io.StringIO()
-            converter = TextConverter(resource_manager, fake_file_handle)
-            page_interpreter = PDFPageInterpreter(resource_manager, converter)
-            page_interpreter.process_page(page)
+    pdf_doc = fitz.open("example.pdf")
 
-            text = fake_file_handle.getvalue()
-            page_num += 1
+    for page in pdf_doc:
+        text = page.getText("text")
 
-            with open("{}/{}_slide.txt".format(extract_dir, page_num), "w") as f:
-                f.write(text)
-
-            # close open handles
-            converter.close()
-            fake_file_handle.close()
+        with open("{}/{}_slide.txt".format(extract_dir, page.number), "w") as f:
+            f.write(text)
 
 
 if __name__ == '__main__':
