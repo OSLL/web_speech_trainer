fe76416d947deb00ccab3ab595bfd0d269b8fd2b
Ваше Имя <you@example.com>
2020-10-15 14:25:42 +0300

    Лемматизация каждого слова на слайде

--- pdf_parser/slide_splitter.py
+++ pdf_parser/slide_splitter.py
@@ -1,4 +1,5 @@
 import fitz
+import pymorphy2
 
 import os
 import argparse
@@ -12,9 +13,18 @@ def extract_text_by_page(pdf_path, extract_dir):
 
     for page in pdf_doc:
         text = page.getText("text")
+        l_text = ''
+        sentences = text.split('\n')
+        morph = pymorphy2.MorphAnalyzer()
+
+        for s in sentences:
+            for word in s.split():
+                l_text += morph.parse(word)[0].normal_form
+                l_text += ' '
+            l_text += '\n'
 
         with open("{}/{}_slide.txt".format(extract_dir, page.number), "w") as f:
-            f.write(text)
+            f.write(l_text)
 
 
 if __name__ == '__main__':
