10c18063de06bde1cd9847de7a6336e8401e9fd2
Ваше Имя <you@example.com>
2020-10-21 23:08:26 +0300

    Добавлена опция очистки txt файлов (Чисто для экспериметнов)

--- pdf_parser/slide_splitter.py
+++ pdf_parser/slide_splitter.py
@@ -1,27 +1,54 @@
 import fitz
 import pymorphy2
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
 
 import os
 import argparse
 
 
-def extract_text_by_page(pdf_path, extract_dir):
+def text_processor(text):
+    l_text = ''
+    sentences = text.split('\n')
+    morph = pymorphy2.MorphAnalyzer()
+    stop_words = stopwords.words('russian')
+
+    # Лемматизация и удаление стоп слов
+    for s in sentences:
+        for word in s.split():
+            normal_form = morph.parse(word)[0].normal_form
+            if normal_form not in stop_words:
+                l_text += normal_form
+                l_text += ' '
+            else:
+                print('Удаляем шумовое слово:', normal_form)
+        l_text += '\n'
+
+    return l_text
+
+
+def parse_txt(txt_path, extract_dir):
     if not os.path.exists(extract_dir):
         os.mkdir(extract_dir)
 
-    pdf_doc = fitz.open("example.pdf")
+    with open(txt_path) as txt:
+        text = txt.readlines()
+        l_text = text_processor(''.join(text))
+
+        with open('{}/clear.txt'.format(extract_dir), 'w') as f:
+            f.write(l_text)
+
+
+def parse_pdf(pdf_path, extract_dir):
+    if not os.path.exists(extract_dir):
+        os.mkdir(extract_dir)
+
+    pdf_doc = fitz.open(pdf_path)
 
     for page in pdf_doc:
         text = page.getText("text")
-        l_text = ''
-        sentences = text.split('\n')
-        morph = pymorphy2.MorphAnalyzer()
-
-        for s in sentences:
-            for word in s.split():
-                l_text += morph.parse(word)[0].normal_form
-                l_text += ' '
-            l_text += '\n'
+        l_text = text_processor(text)
 
         with open("{}/{}_slide.txt".format(extract_dir, page.number), "w") as f:
             f.write(l_text)
@@ -30,7 +57,17 @@ def extract_text_by_page(pdf_path, extract_dir):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--pdf', action="store", dest="pdf")
+    parser.add_argument('--txt', action="store", dest="txt")
     args = parser.parse_args()
 
-    print(extract_text_by_page(args.pdf, args.pdf.split(".")[0]))
+    if args.pdf:
+        parse_pdf(args.pdf, args.pdf.split(".")[0])
+        print('Все готово... Результаты ждут вас в папке', args.pdf.split(".")[0])
+
+    if args.txt:
+        parse_txt(args.txt, args.txt.split(".")[0])
+        print('Все готово... Результаты ждут вас в папке', args.txt.split(".")[0])
+
+
+
 
