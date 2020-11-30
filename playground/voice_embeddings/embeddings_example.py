9642a23632cfa35e735cad5ee4e221176433abc1
ArtemSorokin <artem.sorokin.enm@gmail.com>
2020-12-01 02:03:50 +0300

    add user verification

--- playground/voice_embeddings/embeddings_example.py
+++ playground/voice_embeddings/embeddings_example.py
@@ -12,8 +12,8 @@ SAMPLE_RATE = 22050
 
 
 class Encoder:
-    def __init__(self):
-        self.encoder = VoiceEncoder()
+    def __init__(self, cpu=False):
+        self.encoder = VoiceEncoder() if not cpu else VoiceEncoder('cpu')
 
 
     def load_file(self, filename):
@@ -25,20 +25,18 @@ class Encoder:
         embedding = self.encoder.embed_utterance(wav)
         return embedding
 
-    def embed_full(self, filenames):
+    def embed_full(self, filenames, wavs):
         '''Embed entire files'''
         embeddings = {}
-        for filename in tqdm(filenames):
-            wav = self.load_file(filename)
+        for filename, wav in tqdm(zip(filenames, wavs)):
             embeddings[filename] = self.embed(wav)
         return embeddings
 
-    def embed_windows(self, filenames, window_length: int):
+    def embed_windows(self, filenames, wavs, window_length: int):
         '''Embed overlapping windows of files (length in seconds)'''
         window_length *= SAMPLE_RATE
         embeddings = {}
-        for filename in tqdm(filenames):
-            wav = self.load_file(filename)
+        for filename, wav in tqdm(zip(filenames, wavs)):
             windows = {
                 start: wav[start:start+window_length]
                 for start in range(0, wav.shape[0] - window_length, window_length // 2)
@@ -56,6 +54,34 @@ class Encoder:
             similarity_matrix[j, i] = similarity
         return similarity_matrix
 
+    def pairwise_similarites_list(self, embeddings: list):
+        similarities = []
+        combinations = get_combinations(embeddings)
+        for i, j, e1, e2 in combinations:
+            similarities.append(np.inner(e1, e2))
+        return similarities
+
+    def verify(self, ref_embeddings: dict, embeddings: dict):
+        pairwise_similarities = self.pairwise_similarites_list(embeddings.values())
+        ref_pairwise_similarities = self.pairwise_similarites_list(ref_embeddings.values())
+
+        mutual_similarities = []
+        for ref_embedding in ref_embeddings.values():
+            for embedding in embeddings.values():
+                mutual_similarities.append(np.inner(embedding, ref_embedding))
+
+        ref_quantile = sorted(ref_pairwise_similarities)[int(len(ref_pairwise_similarities) * 0.1)]
+        mutual_quantile = sorted(mutual_similarities)[int(len(mutual_similarities) * 0.9)]
+
+        diff = np.median(ref_pairwise_similarities) - np.median(mutual_similarities)
+
+        # plt.hist(pairwise_similarities, alpha=0.5)
+        # plt.hist(ref_pairwise_similarities, alpha=0.5)
+        # plt.hist(mutual_similarities, alpha=0.5)
+        # plt.show()
+
+        return ref_quantile < mutual_quantile and diff < 0.05
+
 
 def get_combinations(l):
     '''All possible pairs with element indices.'''
@@ -68,18 +94,25 @@ def get_combinations(l):
 
 
 if __name__ == '__main__':
-    encoder = Encoder()
+    encoder = Encoder(cpu=True)
 
-    filenames = [f'data/audio_source_{n}.wav' for n in range(1, 11)][:2]
+    filenames = [f'../data/8000_16_mono_wav/audio_source_{n}.wav' for n in range(1, 11)][:2]
+    wavs = [encoder.load_file(filename) for filename in filenames]
 
-    embeddings = encoder.embed_windows(filenames, 30)
+    # verify
+    print(encoder.verify(encoder.embed_windows(['1a'], [wavs[0][:wavs[0].shape[0] // 4]], 20),
+                         encoder.embed_windows(['1b'], [wavs[0][wavs[0].shape[0] // 3:]], 20)))
+    print(encoder.verify(encoder.embed_windows(['1'], [wavs[0]], 20),
+                         encoder.embed_windows(['2'], [wavs[1]], 20)))
+
+    # compare two recordings
+    embeddings = encoder.embed_windows(filenames, wavs, 30)
     similarity_matrix = encoder.pairwise_similarites(embeddings)
     similarity_df = pd.DataFrame(similarity_matrix)
-    similarity_df.to_csv('similarity_matrix.csv')
-
+    # similarity_df.to_csv('similarity_matrix.csv')
 
     # plot the matrix
-    data = pd.read_csv('similarity_matrix.csv', index_col=0).values
+    similarity_df = pd.read_csv('similarity_matrix.csv', index_col=0).values
 
     fig, ax = plt.subplots()
     ax.matshow(data)
