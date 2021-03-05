c9e416ec5576786010095f387e9e95e95c7268cc
Mark Zaslavskiy <mark.zaslavskiy@gmail.com>
2021-03-05 12:50:58 +0300

    added missing sudo

--- scripts/restart.sh
+++ scripts/restart.sh
@@ -2,7 +2,7 @@
 
 apache_config_filename=${1}
 apache_ssl_mod=${2:-''}
-source scripts/setup_apache_config.sh $apache_config_filename $apache_ssl_mod
+source sudo scripts/setup_apache_config.sh $apache_config_filename $apache_ssl_mod
 
 mkdir -p ../mongo_data
 docker-compose build
