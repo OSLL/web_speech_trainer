a186b9af636269c5c8dbfebad5f2a308972e68b5
Anya18 <unknownladyspb@gmail.com>
2021-04-24 10:48:30 +0300

    fix presentation max size

--- app/static/js/upload.js
+++ app/static/js/upload.js
@@ -1,6 +1,8 @@
+const MAX_PRESENTATION_SIZE = 16;
+
 $('#alert').hide()
 
-function onChange(){
+function onChange() {
     $('#alert').hide()
     if ($('#file-loading').prop('files').length < 1) {
         $('#alert').show()
@@ -9,16 +11,16 @@ function onChange(){
     } else {
         var file = $('#file-loading').prop('files')[0]
         parts = file.name.split('.')
-        if (parts.length <= 1 || parts.pop() != 'pdf'){
-                $('#alert').show()
-                str = "Presentation file should be a pdf file!"
-                $('#error-text').html(str)
-                return
-        }
-        if (file.size > 10000000){
+        if (parts.length <= 1 || parts.pop() != 'pdf') {
             $('#alert').show()
-                str = "Presentation file should not exceed 10MB"
-                $('#error-text').html(str)
+            str = "Presentation file should be a pdf file!"
+            $('#error-text').html(str)
+            return
+        }
+        if (file.size > MAX_PRESENTATION_SIZE *1024*1024) {
+            $('#alert').show()
+            str = `Presentation file should not exceed ${MAX_PRESENTATION_SIZE}MB`
+            $('#error-text').html(str)
         }
     }
 }
