a1c1f46d440f113a32ae00df6755cab3ba23107b
Anya18 <unknownladyspb@gmail.com>
2021-04-27 21:31:10 +0300

    change language

--- app/static/js/upload.js
+++ app/static/js/upload.js
@@ -6,20 +6,20 @@ function onChange() {
     $('#alert').hide()
     if ($('#file-loading').prop('files').length < 1) {
         $('#alert').show()
-        str = "Select file!"
+        str = "Выберете файл!"
         $('#error-text').html(str)
     } else {
         var file = $('#file-loading').prop('files')[0]
         parts = file.name.split('.')
         if (parts.length <= 1 || parts.pop() != 'pdf') {
             $('#alert').show()
-            str = "Presentation file should be a pdf file!"
+            str = "Презентация должна быть в формате PDF!"
             $('#error-text').html(str)
             return
         }
         if (file.size > MAX_PRESENTATION_SIZE *1024*1024) {
             $('#alert').show()
-            str = `Presentation file should not exceed ${MAX_PRESENTATION_SIZE}MB`
+            str = `Размер файла с презентацией не должен привышать ${MAX_PRESENTATION_SIZE}MB`
             $('#error-text').html(str)
         }
     }
