c1581fa8168e7da06d2b2e59628d08e0ca07365d
Evgeny Belousov <northuraldude@gmail.com>
2021-07-13 14:32:32 +0300

    Added IntonationStabilityCriterion class

--- app/criteria.py
+++ app/criteria.py
@@ -2,13 +2,15 @@ import json
 import math
 import re
 import time
-from typing import Optional, Callable
+from typing import Optional, Callable, Tuple
 
 import numpy as np
 import librosa
 import scipy
+import parselmouth
 from bson import ObjectId
 from scipy.spatial.distance import cosine
+from scipy.signal import argrelextrema
 
 from app.audio import Audio
 from app.mongo_odm import DBManager, TrainingsDBManager
@@ -519,3 +521,93 @@ class FillersNumberCriterion(Criterion):
         else:
             verdict = None
         return CriterionResult(1 if fillers_number <= self.parameters['maximum_fillers_number'] else 0, verdict)
+
+# TODO:
+# 1. Refactor class
+# 2. Add Parselmouth package to requirements.txt
+class IntonationStabilityCriterion(Criterion):
+    CLASS_NAME = 'IntonationStabilityCriterion'
+
+    '''
+    Критерий проверяет интонацию речи докладчика на предмет
+    наличия резких скачков интонации.
+    '''
+
+    def __init__(self, parameters, dependent_criteria):
+        super().__init__(
+            name=IntonationStabilityCriterion.CLASS_NAME,
+            parameters=parameters,
+            dependent_criteria=dependent_criteria,
+        )
+
+    def get_pitch_values(self, path) -> np.ndarray:
+        snd = parselmouth.Sound(path)
+        pitch = snd.to_pitch()
+        pitch_values = pitch.selected_array['frequency']
+        pitch_values = list(filter(lambda i: i != 0, pitch_values))
+        pitch_values = np.array(pitch_values)
+
+        return pitch_values
+
+    def get_local_extremas_indexes(self, pitch_values: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+        idx_minimas = argrelextrema(pitch_values, np.less)[0]
+        idx_maximas = argrelextrema(pitch_values, np.greater)[0]
+        idx = np.sort(np.concatenate((idx_minimas, idx_maximas)))
+
+        return idx_minimas, idx_maximas
+
+    @property
+    def description(self):
+        return 'Критерий: {},\nописание: проверяет, стабильна ли интонация докладчика во время ' \
+               'выступления, \nоценка: 0, если не выполнен, 1, если выполнен.\n'.format(self.name)
+
+    def apply(self, audio: Audio, presentation: Presentation, training_id: ObjectId, criteria_results: dict) \
+            -> CriterionResult:
+        current_audio_id = TrainingsDBManager().get_training(training_id).presentation_record_file_id
+        current_audio_file = DBManager().get_file(current_audio_id)
+        try:
+            current_audio_file = convert_from_mp3_to_wav(current_audio_file, frame_rate=self.parameters['sample_rate'])
+        except:
+            return CriterionResult(result=0, verdict='Cannot convert from mp3 to wav')
+        
+        pitch_values = self.get_pitch_values(current_audio_file.name)
+        idx_minimas, idx_maximas = self.get_local_extremas_indexes(pitch_values)
+        
+        minimas = pitch_values[idx_minimas]
+        maximas = pitch_values[idx_maximas]
+        diffs = []
+        critical_diffs = []
+        idx_extremas_critical = []
+        idx_minimas_critical_pitch = []
+        idx_maximas_critical_pitch = []
+
+        idx_size = idx_minimas.size if idx_minimas.size < idx_maximas.size else idx_maximas.size
+        for i in range(0, idx_size):
+            diff = abs(minimas[i] - maximas[i])
+            diffs.append(diff)
+            if (diff >= 75.0):
+                critical_diffs.append(diff)
+                idx_extremas_critical.append(i)
+                idx_minimas_critical_pitch.append(idx_minimas[i])
+                idx_maximas_critical_pitch.append(idx_maximas[i])
+
+        pitch_critical_minimas = np.full((pitch_values.size), fill_value=np.nan)
+        pitch_critical_maximas = np.full((pitch_values.size), fill_value=np.nan)
+        pitch_minimas = np.full((pitch_values.size), fill_value=np.nan)
+        pitch_maximas = np.full((pitch_values.size), fill_value=np.nan)
+
+        for i in range(0, len(pitch_values)):
+            if (i < len(critical_diffs)):
+                pitch_critical_minimas[idx_minimas_critical_pitch[i]] = pitch_values[idx_minimas_critical_pitch[i]]
+                pitch_critical_maximas[idx_maximas_critical_pitch[i]] = pitch_values[idx_maximas_critical_pitch[i]]
+            
+            if (i < len(idx_minimas)):
+                pitch_minimas[idx_minimas[i]] = pitch_values[idx_minimas[i]]
+
+            if (i < len(idx_maximas)):
+                pitch_maximas[idx_maximas[i]] = pitch_values[idx_maximas[i]]
+
+        unstables_num = (pitch_critical_maximas.size + pitch_critical_minimas.size) / 2
+        if (unstables_num >= 10):
+            return CriterionResult(result=0)
+        return CriterionResult(result=1)
