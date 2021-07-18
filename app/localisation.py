1b42af29374b7e85fbcd107c76490bbb1d8795e3
Wintari <dimonx2000.ru@mail.ru>
2021-07-18 06:11:55 +0300

    Localisation functionality was added. Admin page was partially localised.

---
+++ app/localisation.py
@@ -0,0 +1,18 @@
+import i18n
+from flask import Flask
+    
+def loadLocales(path: str):
+    i18n.set('skip_locale_root_data', True)
+    i18n.set('file_format', 'json')
+    i18n.set('filename_format', '{locale}.{format}')
+    i18n.load_path.append(path)
+
+def changeLocale(locale: str, default: str = None):
+    i18n.set('locale', locale)
+    if(default):
+        i18n.set('fallback', default)
+
+def setupTemplatesAlias(app: Flask):
+    app.jinja_env.globals.update(_= i18n.t)
+
+_ = i18n.t
