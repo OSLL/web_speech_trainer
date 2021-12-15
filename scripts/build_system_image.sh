3159cec26067dd90c42c2f43e661a00d5a42f23b
Dmitry Ivanov <darcenrall@gmail.com>
2021-12-15 23:40:48 +0300

    add tag for image

--- scripts/build_system_image.sh
+++ scripts/build_system_image.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+tag=${1:-'v0.1'}
+
 # build vosk
-./scripts/build_image.sh "Dockerfile.kaldi-ru" OSLL/vosk
+./scripts/build_image.sh "Dockerfile.kaldi-ru" osll/vosk:$tag
 
 # build base image
-./scripts/build_image.sh Dockerfile_base OSLL/wst_base
+./scripts/build_image.sh Dockerfile_base osll/wst_base:$tag
