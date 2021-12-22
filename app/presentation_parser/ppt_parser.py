b87da6f688dd30c22014b6bc1088314a50177414
Dmitry Ivanov <darcenrall@gmail.com>
2021-12-22 23:14:00 +0300

    upd pres parsing

--- app/presentation_parser/ppt_parser.py
+++ app/presentation_parser/ppt_parser.py
@@ -3,12 +3,14 @@ from pptx import Presentation
 
 def parse_ppt(presentation_filepath):
     presentation = Presentation(presentation_filepath)
-
     slides = []
     for slide in presentation.slides:
-        slide = { 'title': slide.shapes.title.text if slide.shapes.title else '' }
+        slide_info = {
+            'title': slide.shapes.title.text if slide.shapes.title else '',
+            'words': ''
+        }
         for shape in slide.shapes:
             if hasattr(shape, "text"):
-                slide['words'] += "\n" + shape.text
-        slides.append(slide)
+                slide_info['words'] += "\n" + shape.text
+        slides.append(slide_info)
     return slides
