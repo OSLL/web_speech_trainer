89444b7d71bca0d5a7df9c77479c2690af64ea64
Anna Tarasova <anna_tarasova@epam.com>
2022-04-03 20:32:41 +0300

    Revert "table with results for google docx"
    This reverts commit 8fe15a384e2e9a1de1077cdf62c02990893ac067.

--- app/criteria/speech_and_prezentation_comparasion/comparison.py
+++ app/criteria/speech_and_prezentation_comparasion/comparison.py
@@ -5,6 +5,19 @@ def estimate_by_intersection(main_text, general_text):
     if not isinstance(general_text, set):
         general_text = set(general_text)
 
-    coincidence = len(main_text.intersection(general_text))
+    concurrence = len(main_text & general_text)
 
-    return 0 if len(main_text) == 0 else coincidence / len(main_text)
+    return 0 if len(main_text) == 0 else concurrence / len(main_text)
+
+
+def estimate_by_comparing_keywords(main_text, general_text_keywords, general_text_all):
+    if not isinstance(main_text, set):
+        main_text = set(main_text)
+    if not isinstance(main_text, set):
+        general_text_keywords = set(general_text_keywords)
+    general_text_add = set(general_text_all).difference(general_text_keywords)
+
+    concurrence = len(main_text & general_text_keywords)
+    difference = len(main_text & general_text_add)
+
+    return 0 if concurrence == 0 else concurrence / (concurrence + difference)
