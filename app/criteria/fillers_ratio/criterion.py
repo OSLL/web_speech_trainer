4f2a3be22ce6e8198041067ae716da98dd7d0bc2
severus2293 <71723580+severus2293@users.noreply.github.com>
2023-07-10 12:47:43 +0300

    Update criterion.py

--- app/criteria/fillers_ratio/criterion.py
+++ app/criteria/fillers_ratio/criterion.py
@@ -27,7 +27,6 @@ class FillersRatioCriterion(BaseCriterion):
         "Критерий": t(self.name),
         'Описание':t('проверяет, что в речи нет слов-паразитов, используются слова из списка {}').format(self.parameters['fillers']),
         'Оценка':t('(1 - доля слов-паразитов)'),
-        'Вес':''
         } 
 
 
