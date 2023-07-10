93c9f5708010335f4356a0ded2b58db412b19108
severus2293 <71723580+severus2293@users.noreply.github.com>
2023-07-10 13:08:38 +0300

    Update trainings.py

--- app/routes/trainings.py
+++ app/routes/trainings.py
@@ -126,28 +126,11 @@ def view_all_trainings():
         or an empty dictionary if access was denied.
     """
     username = request.args.get('username', '')
-
-    page = 0
-    count = 10
-
-    try:
-        page = int(request.args.get('page', '0'))
-    except:
-        pass
-
-    try:
-        count = int(request.args.get('count', '10'))
-    except:
-        pass
-
+    full_name = request.args.get('full_name', '')
     authorized = check_auth() is not None
     if not (check_admin() or (authorized and session.get('session_id') == username)):
         return {}, 404
-
-    raw_filters = request.args.getlist('f')
-    filters_string = '&'.join(raw_filters)
-
-    return render_template('show_all_trainings.html', username=username, filters=filters_string, is_admin="true" if check_admin() else 'false', page=str(page), count=str(count)), 200
+    return render_template('show_all_trainings.html', username=username, full_name=full_name, is_admin="true" if check_admin() else 'false'), 200
 
 
 @routes_trainings.route('/training_greeting/', methods=['GET'])
