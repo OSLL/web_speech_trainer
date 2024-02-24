142111c49252092ec4470d79bc4caa6c94c705f7
Vadim Arkhipov <arkhipov_vadim@inbox.ru>
2024-02-24 23:48:19 +0300

    update whisper test RTF

--- tests/test_whisper.py
+++ tests/test_whisper.py
@@ -1,28 +1,32 @@
-import sys
 import requests
 import time
 import librosa
 
-url = "http://0.0.0.0:9000/asr?encode=true&task=transcribe&word_timestamps=true&output=json"
-headers = {'accept': 'application/json'}
+def test_whisper(file):
+    url = "http://whisper:9000/asr"
+    params = {
+        'task': 'transcribe',
+        'language': 'ru',
+        'word_timestamps': 'true',
+        'output': 'json'
+    }
+    headers = {'accept': 'application/json'}
+    print(f"Processing file \"{file}\"")
+    files = {'audio_file': (file, open(file, 'rb'), 'audio/mpeg')}
 
-file = sys.argv[1]
-print(f"Processing file \"{file}\"")
-files = {'audio_file': (file, open(file, 'rb'), 'audio/mpeg')}
+    audio_length = librosa.get_duration(path=file)
 
-audio_length = librosa.get_duration(path=file)
+    start_time = time.time()
+    response = requests.post(url, params=params, headers=headers, files=files)
+    end_time = time.time()
+    processing_time = end_time - start_time
+    RTF = processing_time / audio_length
+    print(f"RTF = {RTF}")
 
-start_time = time.time()
-response = requests.post(url, headers=headers, files=files)
-end_time = time.time()
-processing_time = end_time - start_time
-RTF = processing_time / audio_length
-print(f"RTF = {RTF}")
-
-#parsing
-data = response.json()
-words = []
-for segment in data["segments"]:
-    for word_structure in segment["words"]:
-        words.append(word_structure)
-        print(word_structure)
+    #parsing
+    data = response.json()
+    words = []
+    for segment in data["segments"]:
+        for word_structure in segment["words"]:
+            words.append(word_structure)
+            print(word_structure)
