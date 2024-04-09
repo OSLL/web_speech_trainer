13595e40f51fdef00d5f7e323b8016b933b47c9b
Vadim Arkhipov <arkhipov_vadim@inbox.ru>
2024-04-09 21:24:42 +0300

    fix whisper tests

--- tests/test_whisper.py
+++ tests/test_whisper.py
@@ -10,10 +10,10 @@ def test_whisper():
     Config.init_config('../app_conf/testing.ini')
     whisper = WhisperAudioRecognizer(Config.c.whisper.url)
 
-    audio_to_recognize = open("./simple_phrases_russian.wav", "rb")
+    audio_to_recognize = open("./simple_phrases_russian.mp3", "rb")
     res = whisper.recognize(audio_to_recognize)
     print(list(map(lambda x: x.word.value.lower(), res.recognized_words)))
 
-    audio_to_recognize = open("./simple_phrases_russian.wav", "rb")
+    audio_to_recognize = open("./simple_phrases_russian.mp3", "rb")
     res = whisper.send_audio_to_recognizer(audio_to_recognize)
     print(list(map(lambda x: x["word"].lower(), res)))
