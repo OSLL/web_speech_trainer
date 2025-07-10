05b341f1b10e7e4a4327504acf002dfe51353bf1
PeeachPie <nestandrem@gmail.com>
2025-07-10 17:46:37 +0300

    Новая структура селениум тестов

--- tests/selenium/conftest.py
+++ tests/selenium/conftest.py
@@ -6,12 +6,10 @@ from app.config import Config
 CONFIG_PATH = f'{ROOT_DIR}/app_conf/testing.ini'
 AUDIO_FILE = f"{ROOT_DIR}/simple_phrases_russian.wav"
 
-@pytest.fixture(scope="class")
-def selenium_session(request):
+@pytest.fixture(scope="module")
+def selenium_session():
     Config.init_config(CONFIG_PATH)
     session = SeleniumSession(Config.c, chrome_options(AUDIO_FILE))
-
-    request.cls.selenium_session = session
 
     yield session
 
