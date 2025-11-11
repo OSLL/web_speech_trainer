328972f2e00607dd9e8808498de4eaeab7357d2e
Shumer-1 <borisov1908vlad@mail.ru>
2025-11-11 23:02:14 +0300

    added stub

--- app/routes/interview.py
+++ app/routes/interview.py
@@ -7,3 +7,17 @@ def interview_page():
     return render_template(
         'interview.html'
     ), 200
+
+
+@routes_interview.route("/avatar_stream")
+def avatar_stream():
+    def generate():
+        frame = np.zeros((480, 640, 3), dtype=np.uint8)
+        _, buffer = cv2.imencode('.jpg', frame)
+        jpg_bytes = buffer.tobytes()
+
+        while True:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + jpg_bytes + b'\r\n')
+
+    return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
